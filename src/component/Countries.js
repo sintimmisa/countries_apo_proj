9a2b520536c2af670f8db44ecffd19e927603e8c
Godfred D. Sintim-Misah <sintim.misa@gmail.com>
2023-11-21 23:43:13 +0000

    fethc data from api

--- src/component/Countries.js
+++ src/component/Countries.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
 
 const Countries = () => {
-  return (
-    <div>Countries</div>
-  )
-}
+  //Manage state
 
-export default Countries
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const getCountries = async () => {
+      try {
+        const res = await fetch("https://restcountries.com/v3.1/all");
+        const data = await res.json();
+        setCountries(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getCountries();
+  }, []);
+  return <div>{countries.length}</div>;
+};
+
+export default Countries;
